3ed5c436e9f295245feaeffcd7e6a427643a98d4
McCarthyDe <mcde8683@colorado.edu>
2026-04-07 10:17:46 -0600

    Add files via upload

---
+++ AircraftEOM.m
@@ -0,0 +1,82 @@
+function xdot = AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_inertial, aircraft_parameters)
+
+Ix = aircraft_parameters.Ix;
+Iy = aircraft_parameters.Iy;
+Iz = aircraft_parameters.Iz;
+Ixz = aircraft_parameters.Ixz;
+
+xdot = zeros(12, 1);
+
+x_E = aircraft_state(1);
+y_E = aircraft_state(2);
+z_E = aircraft_state(3);
+phi = aircraft_state(4);
+theta = aircraft_state(5);
+psi = aircraft_state(6);
+u_E = aircraft_state(7);
+v_E = aircraft_state(8);
+w_E = aircraft_state(9);
+p = aircraft_state(10);
+q = aircraft_state(11);
+r = aircraft_state(12);
+
+A1 = [cos(theta)*cos(psi), (sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi)), (cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi));
+    cos(theta)*sin(psi), (sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi)), (cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi));
+    -sin(theta), sin(phi)*cos(theta), cos(phi)*cos(theta)];
+
+A2 = [1, sin(phi)*tan(theta), cos(phi)*tan(theta);
+    0, cos(phi), -sin(phi);
+    0, sin(phi)*sec(theta), cos(phi)*sec(theta)];
+
+A3 = [(r*v_E - q*w_E), (p*w_E - r*u_E), (q*u_E - p*v_E)]';
+
+A4 = [-sin(theta), cos(theta)*sin(phi), cos(theta)*cos(phi)]';
+
+% Lambdas
+Lam = Ix*Iz-Ixz^2;
+L1 = (Ixz*(Ix-Iy+Iz)/Lam);
+L2 = (Iz*(Iz-Iy)+Ixz^2)/Lam;
+L3 = Iz/Lam;
+L4 = Ixz/Lam;
+L5 = (Iz-Ix)/Iy;
+L6 = Ixz/Iy;
+L7 = (Ix*(Ix-Iy)+Ixz^2)/Lam;
+L8 = Ix/Lam;
+
+A5 = [L1*p*q-L2*q*r, L5*p*r-L6*(p^2-r^2), L7*p*q-L1*q*r]';
+
+rho = stdatmo(z_E);
+[aero_forces, aero_moments] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, rho, aircraft_parameters);
+
+X = aero_forces(1);
+Y = aero_forces(2);
+Z = aero_forces(3);
+L = aero_moments(1);
+M = aero_moments(2);
+N = aero_moments(3);
+
+A6 = [X,Y,Z]';
+
+A7 = [L3*L+L4*N, M/Iy, L4*L+L8*N]';
+
+var_dot_XYZ = A1*[u_E, v_E, w_E]';
+var_dot_O = A2*[p, q, r]';
+var_dot_UVW = A3 + aircraft_parameters.g*A4 + (1/aircraft_parameters.m)*A6;
+var_dot_PQR = A5 + A7;
+
+xdot(1) = var_dot_XYZ(1);
+xdot(2) = var_dot_XYZ(2);
+xdot(3) = var_dot_XYZ(3);
+
+xdot(4) = var_dot_O(1);
+xdot(5) = var_dot_O(2);
+xdot(6) = var_dot_O(3);
+
+xdot(7) = var_dot_UVW(1);
+xdot(8) = var_dot_UVW(2);
+xdot(9) = var_dot_UVW(3);
+
+xdot(10) = var_dot_PQR(1);
+xdot(11) = var_dot_PQR(2);
+xdot(12) = var_dot_PQR(3);
+end
