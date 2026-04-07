00136c70996b8da2e84f4dae09f411dfb745fa32
cselBum7 <130393890+cselBum7@users.noreply.github.com>
2026-04-07 09:44:25 -0600

    templates for task 2 things

---
+++ LabTask2p3.m
@@ -0,0 +1,42 @@
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Goal: 
+%    - Check an actual state and control vector
+% Comments: 
+%   - not a trim state
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% initial state vector 
+x0 = [0,0,-1800,15,-12,270,19,3,-2,.08,-.2,0]'; % NOTE: units are [m],[rad],[m\s],[deg/s]
+
+% initial control vector
+u0 = [5,2,-13,.3]'; % degrees
+
+% call to EOM function 
+
+% figure numbers 
+figNums = 1:6; 
+
+
+% call to plot aircraftsim
+PlotAircraftSim()
+
+% saving figures 
+save = 0; 
+
+if save
+    folder = sprintf('LabTask2p3'); % change the name of the folder per task. The i, is if there are multiple cases
+    if ~exist(folder, 'dir')
+        mkdir(folder);
+    end
+
+       % will put call to plot aircraft sim in here after it works 
+
+        % Save each figure
+    for k = 1:length(figNums)
+        f = figure(current_figs(k));   % grab figure handle
+
+        filename = sprintf('%s/Fig_%d.png', folder, k);
+        exportgraphics(f, filename, 'Resolution', 300);
+    end
+
+end
